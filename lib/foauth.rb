b4381b2e3f0e4bffcbd5e9c3935c1e5157e2da1a
Chris Mytton <self@hecticjeff.net>
2013-01-03 21:09:33 +0000

    Add module method for creating a client
    This makes the project feature complete with the README

--- lib/foauth.rb
+++ lib/foauth.rb
@@ -1,3 +1,9 @@
 require 'foauth/version'
 require 'foauth/rewrite_middleware'
 require 'foauth/client'
+
+module Foauth
+  def self.new(*args, &block)
+    Foauth::Client.new(*args, &block)
+  end
+end
