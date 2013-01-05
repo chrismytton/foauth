d8e6abc689cb4f5336524da24a12ff8989ceff0b
Chris Mytton <self@hecticjeff.net>
2013-01-05 16:52:01 +0000

    Remove block variable

--- lib/foauth.rb
+++ lib/foauth.rb
@@ -4,12 +4,12 @@ require 'foauth/version'
 require 'foauth/rewrite_middleware'
 
 module Foauth
-  def self.new(email, password, &block)
+  def self.new(email, password)
     Faraday.new do |builder|
       builder.request :basic_auth, email, password
       builder.use RewriteMiddleware
       builder.adapter Faraday.default_adapter
-      block.call(builder) if block_given?
+      yield builder if block_given?
     end
   end
 end
